778e675743542263a089b2f697997f5f8d6edca3
Fredianto <47935148+frediness@users.noreply.github.com>
2019-05-21 14:00:11 +0700

    Add files via upload

---
+++ queue-in-python/queue1.py
@@ -0,0 +1,28 @@
+class PriorityQueue(object):
+    def __init__(self):
+        self.qlist = []
+    def isEmpty(self):
+        return len(self) == 0
+    def __len__(self):
+        return len(self.qlist)
+    def enqueue(self, data, priority):
+        entry = _PriorityQEntry(data, priority)
+        self.qlist.append(entry)
+    def dequeue(self):
+        self.qlist.pop()
+    def getFrontMost(self):
+        return self.qlist[0]
+    def getRearMost(self):
+        return self.qlist[-1]
+
+class _PriorityQEntry(object):
+    def __init__(self, data, priority):
+        self.data = data
+        self.priority = priority
+        
+S = PriorityQueue()
+S.enqueue("Jeruk", 4)
+S.enqueue("Tomat", 2)
+S.enqueue("Mangga", 0)
+S.enqueue("Duku", 5)
+S.enqueue("Pepaya", 2)
